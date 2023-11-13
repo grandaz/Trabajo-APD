58b73d64b4e1ddd1e86f3ce0a1ba2e592c34ed6c
grandaz <20200945@aloe.ulima.edu.pe>
2023-11-13 17:59:39 -0500

    val-cruzada

--- Script.R
+++ Script.R
@@ -137,10 +137,6 @@ boxplot(heart_data$weight, main = "Boxplot de Variable Peso")
 heart_data[, c('age', 'height', 'weight')] <- scale(heart_data[, c('age', 'height', 'weight')])
 
 
-#------------------------------------------------------------------------------#
-# Regresión Logística                                                          #                                                
-#------------------------------------------------------------------------------#
-
 # Factorizar los variables binarias y categóricas
 heart_data$gender <- factor(heart_data$gender)
 heart_data$cholesterol <- factor(heart_data$cholesterol, ordered = TRUE)
@@ -148,13 +144,130 @@ heart_data$gluc <- factor(heart_data$gluc, ordered = TRUE)
 heart_data$smoke <- factor(heart_data$smoke)
 heart_data$alco <- factor(heart_data$alco)
 heart_data$active <- factor(heart_data$active)
-#heart_data$cardio <- factor(heart_data$cardio)  # Variable dependiente
 heart_data$high_bp <- factor(heart_data$high_bp)
 
+#heart_data$cardio <- factor(heart_data$cardio)  # Variable dependiente
+heart_data$cardio <- as.factor(make.names(heart_data$cardio))
+
+#------------------------------------------------------------------------------#
+# Validación Cruzada (CV K-Fold)                                               #                                                
+#------------------------------------------------------------------------------#
+
+# Establecer semilla para reproducibilidad
+set.seed(123)
+
+# División de los datos
+library(caret)
+
+# Crea una partición (70% entrenamiento, 15% validación, 15% prueba)
+indices_entrenamiento <- createDataPartition(y = heart_data$cardio, p = 0.7, list = FALSE)
+train_data <- heart_data[indices_entrenamiento, ]
+conjunto_temporal <- heart_data[-indices_entrenamiento, ]
+
+# Crear partición en el conjunto temporal para obtener validación y prueba (50% cada uno)
+indices_temporales <- createDataPartition(y = conjunto_temporal$cardio, p = 0.5, list = FALSE)
+validation_data <- conjunto_temporal[indices_temporales, ]
+test_data <- conjunto_temporal[-indices_temporales, ]
+
+# Definimos indicadores
+# Puede ser "Accuracy",   "logLoss", "ROC",   "Kappa"
+metrica <- "ROC"
+
+# Definimos parámetros para la Validación Cruzada
+particiones  <- 5
+repeticiones <- 10
+
+# Definir la configuración de control para la Validación Cruzada
+ctrl <- trainControl(method = "repeatedcv", number = particiones,
+                              repeats = repeticiones,
+                              #seeds = seeds,
+                              classProbs = TRUE,
+                              summaryFunction = twoClassSummary,
+                              returnResamp = "final", verboseIter = FALSE,
+                              allowParallel = TRUE)
+
+#------------------------------------------------------------------------------#
+# Entrenamos el modelo de Regresión Logística                                  #                                                
+#------------------------------------------------------------------------------#
+
+logistic_hiperparametros <- data.frame(parameter = "none")
+
+logistic_model <- train(cardio ~ ., data = train_data,
+                         method = "glm",
+                         tuneGrid = logistic_hiperparametros,
+                         metric = metrica,
+                         trControl = ctrl,
+                         family = "binomial")
+logistic_model
+logistic_model$results
+logistic_model$resample
+summary(logistic_model$finalModel)
+
+#------------------------------------------------------------------------------#
+# Entrenamos el modelo de Árbol de Decisión                                    #                                                
+#------------------------------------------------------------------------------#
+
+# Define la cuadrícula de ajuste para el parámetro cp
+hiperparametros_tree <- expand.grid(cp = seq(0.01, 0.5, by = 0.01))
+
+# Entrenar el modelo de árbol de decisión
+tree_model <- train(cardio ~ ., data = train_data,
+                     method = "rpart",
+                     tuneGrid = hiperparametros_tree,
+                     metric = metrica,
+                     trControl = ctrl)
+tree_model
+tree_model$results
+tree_model$resample
+summary(tree_model$finalModel)
+
+# Visualizar el árbol de decisión con rpart.plot
+library(rpart.plot)
+prp(tree_model$finalModel, type = 2, extra = 1, main = "Árbol de Decisión Binario")
 
 
+#------------------------------------------------------------------------------#
+# Validación de los modelos                                                    #                                                
+#------------------------------------------------------------------------------#
+
+library(pROC)
+
+# Predecir las probabilidades en los datos de validación
+logistic_pred <- predict(logistic_model, newdata = validation_data, type = "prob")[,2]
+tree_pred <- predict(tree_model, newdata = validation_data, type = "prob")[,2]
+
+# Crear objetos roc para calcular el área bajo la curva ROC
+logistic_roc <- roc(validation_data$cardio, logistic_pred)
+tree_roc <- roc(validation_data$cardio, tree_pred)
+
+# Comparar las áreas bajo la curva ROC
+roc.test(logistic_roc, tree_roc)
+
+# Visualizar las curvas ROC
+plot(logistic_roc, col = "blue", main = "Curvas ROC de Modelos",
+     lwd = 2, col.main = "black", ylim = c(0, 1))
+lines(tree_roc, col = "red", lwd = 2)
+legend("bottomright", legend = c("Regresión Logística", "Árbol de Decisión"),
+       col = c("blue", "red"), lwd = 2)
+
+# Calcular el AUC para ambos modelos
+auc_logistic <- auc(logistic_roc)
+auc_tree <- auc(tree_roc)
+
+# Imprimir los resultados
+cat("AUC para Regresión Logística:", auc_logistic, "\n")
+cat("AUC para Árbol de Decisión:", auc_tree, "\n")
 
 
+# Factorizar los resultados de la predicción según el nombre
+logistic_pred_binary <- factor(ifelse(logistic_pred > 0.5, "X1", "X0"), levels = levels(validation_data$cardio))
+tree_pred_binary <- factor(ifelse(tree_pred > 0.5, "X1", "X0"), levels = levels(validation_data$cardio))
 
 
+# Construir las matrices de confusión
+confusion_matrix_logistic <- confusionMatrix(data = logistic_pred_binary, reference = validation_data$cardio)
+confusion_matrix_tree <- confusionMatrix(data = tree_pred_binary, reference = validation_data$cardio)
 
+# Mostrar las matrices de confusión
+print(confusion_matrix_logistic)
+print(confusion_matrix_tree)
